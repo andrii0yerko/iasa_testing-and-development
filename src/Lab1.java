ba6da3ab85b59cd88e2b00e904b7d1535516d029
Andrii Yerko <aerko12@gmail.com>
2020-11-19 00:15:44 +0200

    Add getFirstNonRepeatingLetter function (Task №2)
    Added getFirstNonRepeatingLetter function and a few unit tests in main
    for it.

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,5 +1,6 @@
 package src;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.ArrayList;
@@ -24,11 +25,37 @@ public class Lab1 {
 		return integers;
 	}
 	
+	/**
+	 * Task №2
+	 * returns the first non-repeating character (case-insensitive for letters) of the input string
+	 * @param string 
+	 * @return the first character that is not repeated anywhere in this string.
+	 */
+	public static char getFirstNonRepeatingLetter(String input) {
+		String lowercase = input.toLowerCase(); // used to compare letters in same case
+		for (int i=0; i < input.length(); i++) {
+			boolean repeating = false;
+			for (int j=0; j < input.length(); j++) { //searching for same character
+				if (i == j) continue; // except itself
+				if (lowercase.charAt(i) == (lowercase.charAt(j))) {
+					repeating = true;
+					break;
+				}
+			}
+			if (!repeating) return input.charAt(i);
+		}
+		return (Character) null;
+	}
+	
 	public static void main(String[] args) {
 		// Tests for task 1
 		assertTrue(getIntegersFromList(Arrays.asList(1, 2, "a", "b")).equals(Arrays.asList(1, 2)));
 		assertTrue(getIntegersFromList(Arrays.asList(1, 2, "a", "b", 0, 15)).equals(Arrays.asList(1, 2, 0, 15)));
 		assertTrue(getIntegersFromList(Arrays.asList(1, 2, "a", "b", "aasf", "1", "123", 231)).equals(Arrays.asList(1, 2, 231)));
+		
+		// Tests for task 2
+		assertEquals(getFirstNonRepeatingLetter("stress"), 't');
+		assertEquals(getFirstNonRepeatingLetter("sTreSS"), 'T');
 	}
 
 }
